55edab4891bef7ba1386999b82004007ffec6c3b
chirag627 <chiragagrawal627@gmail.com>
2022-07-16 11:38:26 +0530

    First Assisted Practice Program

---
+++ assisted practice/src/explicitAndImplicit.java
@@ -0,0 +1,14 @@
+
+public class explicitAndImplicit {
+public static void main(String[] args) {
+	//implicit type Conversion
+	byte b=8;
+	int x=b;
+	System.out.println(x);
+	
+	//Explicit Type Conversion
+	int a=80;
+	byte z=(byte) a;
+	System.out.println(z);
+}
+}
